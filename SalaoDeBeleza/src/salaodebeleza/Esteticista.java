f13a315aab51ed079f8b9f680e3c9318bc5bcf97
31324673 <31324673@099054-P31S204.macklabs.br>
2017-05-10 08:59:38 -0300

    classe esteticista

---
+++ SalaoDeBeleza/src/salaodebeleza/Esteticista.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salaodebeleza;
+
+/**
+ *
+ * @author 31324673
+ */
+public class Esteticista extends Pessoa{
+    
+    public Esteticista(String nome, String endereco, String cidade, int telefone) {
+        super(nome, endereco, cidade, telefone);
+    }
+    protected String especialidade;
+    protected  String codigoFuncionario;
+
+    public Esteticista(String especialidade, String codigoFuncionario, String nome, String endereco, String cidade, int telefone) {
+        super(nome, endereco, cidade, telefone);
+        this.especialidade = especialidade;
+        this.codigoFuncionario = codigoFuncionario;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getCodigoFuncionario() {
+        return codigoFuncionario;
+    }
+
+    public void setCodigoFuncionario(String codigoFuncionario) {
+        this.codigoFuncionario = codigoFuncionario;
+    }
+    
+    public efetuarTratamento(){
+        
+    }
+    
+}
