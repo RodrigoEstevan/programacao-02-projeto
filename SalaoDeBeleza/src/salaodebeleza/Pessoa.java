526fbb145cdd7983df6a58b9ed0c18607684462b
31324673 <31324673@099054-P31S204.macklabs.br>
2017-05-10 08:47:25 -0300

    classe pessoa

---
+++ SalaoDeBeleza/src/salaodebeleza/Pessoa.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salaodebeleza;
+
+/**
+ *
+ * @author 31324673
+ */
+public abstract class Pessoa {
+    protected String nome;
+    protected String endereco;
+    protected  String cidade;
+    protected int telefone;
+
+    public Pessoa(String nome, String endereco, String cidade, int telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.telefone = telefone;
+    }
+    
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+    
+    
+}
