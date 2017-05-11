90c14d1b25c12d346bde4a717bcaecc56626a4e7
31324673 <31324673@099205-P31S202.macklabs.br>
2017-05-11 09:42:37 -0300

    inserçaão de leitor de arquivos

--- SalaoDeBeleza/src/salaodebeleza/SalaoDeBeleza.java
+++ SalaoDeBeleza/src/salaodebeleza/SalaoDeBeleza.java
@@ -5,6 +5,11 @@
  */
 package salaodebeleza;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author 41646843
@@ -15,7 +20,38 @@ public class SalaoDeBeleza {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      Scanner ler = new Scanner(System.in);
+   
+   
+   
+   
+   
+    System.out.printf("Informe o nome de arquivo texto:\n");
+    String nome = ler.nextLine();
+ 
+    System.out.printf("\nConteúdo do arquivo texto:\n");
+    try {
+      FileReader arq = new FileReader(nome);
+      BufferedReader lerArq = new BufferedReader(arq);
+ 
+      String linha = lerArq.readLine(); 
+      while (linha != null) {
+        System.out.printf("%s\n", linha);
+        System.out.println("\n");
+ 
+        linha = lerArq.readLine(); 
+      }
+ 
+      arq.close();
+    } catch (IOException e) {
+        System.err.printf("Erro na abertura do arquivo: %s.\n",
+          e.getMessage());
     }
+ 
+    System.out.println();
+
+
+    }
+    
     
 }
