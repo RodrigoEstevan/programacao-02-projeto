77d75bfe8489ab41d11c847b4630dd859989f34e
31623913 <31623913@099064-P31S202.macklabs.br>
2017-05-17 09:19:40 -0300

    correção leitura de arquivos no main 2

--- SalaoDeBeleza/src/salaodebeleza/SalaoDeBeleza.java
+++ SalaoDeBeleza/src/salaodebeleza/SalaoDeBeleza.java
@@ -18,24 +18,24 @@ public class SalaoDeBeleza {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         System.out.printf("Informe o nome de arquivo texto:\n");
-    String nome = ler.nextLine();
+        String nome = ler.nextLine();
  
-    System.out.printf("\nConteúdo do arquivo texto:\n");
-    try {
-      FileReader arq = new FileReader(nome);
-      BufferedReader lerArq = new BufferedReader(arq);
+        System.out.printf("\nConteúdo do arquivo texto:\n");
+            try {
+                FileReader arq = new FileReader(nome);
+                BufferedReader lerArq = new BufferedReader(arq);
  
-      String linha = lerArq.readLine(); 
-      while (linha != null) {
-        System.out.printf("%s\n", linha);
+                String linha = lerArq.readLine(); 
+                while (linha != null) {
+                System.out.printf("%s\n", linha);
  
-        linha = lerArq.readLine(); 
+                linha = lerArq.readLine(); 
       }
  
-      arq.close();
-    } catch (IOException e) {
-        System.err.printf("Erro na abertura do arquivo: %s.\n",
-          e.getMessage());
+                arq.close();
+            } catch (IOException e) {
+                System.err.printf("Erro na abertura do arquivo: %s.\n",
+                e.getMessage());
     }
  
     System.out.println();
